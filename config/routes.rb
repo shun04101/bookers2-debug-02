851200c8131948862dbbdb89f2fd09922206f684
EC2 Default User <ec2-user@ip-172-31-94-136.ec2.internal>
2021-11-23 05:16:00 +0000

    [Finish]応用課題③(コメント・いいね機能実装)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
+
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+
 end
